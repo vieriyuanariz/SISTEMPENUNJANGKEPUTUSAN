603435f15c6687633da9aa7e90a34905f21291a1
Vieri889 <vieriyuanariz@gmail.com>
2021-01-09 03:03:59 +0700

    publish file index

---
+++ main.py
@@ -0,0 +1,10 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import json
+
+with open("intents.json") as file:
+	data = json.load(file)
+
+print(data["intents"])
